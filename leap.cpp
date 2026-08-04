1912985f3af8c21d366b07ac9a038233978ab702
swapnaliveer202-hash <swapnaliveer202@gmail.com>
2026-08-04 07:15:35 +0000

    change

---
+++ leap.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{ 
+ int year;
+ 
+ cout<<"Enter a year";
+ cin>>year;
+ if((year%400==0)||(year%4==0&&year%100!=0))
+ cout<<"Leap Year";
+ else
+ cout<<"Not Leap Year";
+ return 0;
+}
