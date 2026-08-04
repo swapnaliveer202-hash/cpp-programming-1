65c52ccfc46ca1593956ae0c508ac24daff4f07a
swapnaliveer202-hash <swapnaliveer202@gmail.com>
2026-08-04 07:26:41 +0000

    change

---
+++ fact.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n;
+ long long fact=1;
+ cout<<"Enter a number";
+ cin>>n;
+ for(int i=1;i<=n;i++)
+ {
+ fact=fact*i;  
+ }
+ cout<<"factorial="<<fact;
+ return 0;
+}
+ 
+
